0423cd7e3ed828723755d06a2d874c75ec3c604d
0zcl <1274828542@qq.com>
2018-04-26 18:41:34 +0800

    add view and url

--- Workspace/blogproject/blog/views.py
+++ Workspace/blogproject/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'blog/index.html', context={
+                      'title': '我的博客首页',
+                      'welcome': '欢迎访问我的博客首页'
+                  })
