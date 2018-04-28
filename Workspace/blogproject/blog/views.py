6d822fd946da76700876b361a74131e8a9b71590
0zcl <1274828542@qq.com>
2018-04-28 19:20:57 +0800

    add category class

--- Workspace/blogproject/blog/views.py
+++ Workspace/blogproject/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 
 # Create your views here.
-from .models import Post
+from .models import Post, Category
 import markdown
 
 
@@ -21,3 +21,16 @@ def detail(request, pk):
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
 
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
